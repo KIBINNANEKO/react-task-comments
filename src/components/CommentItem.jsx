c0763492b39a78282168bbd62d237478b1cc470f
Kostiantyn Ivanov <kostia10kot@gmail.com>
2023-06-22 17:44:43 +0300

    fix: fixed functional of randomize avatar's color

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,23 +1,38 @@
 import { generateRandomColor } from "../utils/functions"
+import { useDispatch } from "react-redux"
+import { removeComment } from "../redux/commentsActions"
+import { useEffect, useState } from "react"
 
 
 function CommentItem(props){
 
-	const {body, user} = props
+	const {body, user, id} = props
+
+	const dispatch = useDispatch()
+
+	const [avatarColor, setAvatarColor] = useState("")
+
+	useEffect(() => {
+		setAvatarColor(generateRandomColor())
+	}, [])
+
+	function deleteComment(id){
+		dispatch(removeComment(id))
+	}
 
 	return(
-	<div className="comment-item">
-		<div className="comment-item__avatar-block">
-				<div className="comment-item__avatar" style={{'border': `1px solid ${generateRandomColor()}`}}>
-					<span className="comment-item__avatar-text">{user.username[0]}</span>
-				</div>
+		<div className="comment-item">
+			<div className="comment-item__avatar-block">
+					<div className="comment-item__avatar" style={{ 'border': `1px solid ${avatarColor}`}}>
+						<span className="comment-item__avatar-text">{user.username[0]}</span>
+					</div>
+			</div>
+			<div className="comment-item__body-block">
+				<p className="comment-item__nickname">{user.username}</p>
+				<p className="comment-item__comment">{body}</p>
+				<button className="comment-item__remove-button" onClick={() => deleteComment(id)}>Delete</button>
+			</div>
 		</div>
-		<div className="comment-item__body-block">
-			<p className="comment-item__nickname">{user.username}</p>
-			<p className="comment-item__comment">{body}</p>
-			<button className="comment-item__remove-button">Delete</button>
-		</div>
-	</div>
 	)
 }
 
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,15 @@
 export function generateRandomColor() {
-	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-	return `#${randomColor}`;
+	let randomColor = ''
+
+	do {
+		randomColor = Math.floor(Math.random() * 16777215).toString(16)
+	} while (isWhiteOrLightGray(randomColor))
+
+	return `#${randomColor}`
 }
 
+function isWhiteOrLightGray(color) {
+	const threshold = 0xCCCCCC
+	const numericColor = parseInt(color, 16)
+	return numericColor >= threshold
+}
